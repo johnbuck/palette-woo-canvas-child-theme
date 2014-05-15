ee7454717b6421265c91df5a9fbd2b217b272f13
John Buckingham <johnmbuckingham@gmail.com>
2014-05-15 09:58:11 -0700

    Make WooSlider optional
    Thomas Griffin's plugin activation class was originally set to require WooSlider and force activation. I've removed the slider from this repo, and therefore made it optional to install.

--- includes/palette-extend-canvas-plugins.php
+++ includes/palette-extend-canvas-plugins.php
@@ -46,7 +46,7 @@ function palette_canvas_register_required_plugins() {
             'source'             => get_stylesheet_directory() . '/assets/plugins/wooslider.zip', // The plugin source.
             'required'           => false, // If false, the plugin is only 'recommended' instead of required.
             'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
-            'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+            'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
             'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
             'external_url'       => '', // If set, overrides default API URL and points to an external URL.
         ),
